backwardn/wireguard-android

databinding: rewrite in kotlin

ui/src/main/java/com/wireguard/android/databinding/BindingAdapters.kt
@@ -1,0 +1,135 @@
+/*
+ * Copyright © 2017-2019 WireGuard LLC. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package com.wireguard.android.databinding
+
+import android.text.InputFilter
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ObservableList
+import androidx.databinding.ViewDataBinding
+import androidx.databinding.adapters.ListenerUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.wireguard.android.BR
+import com.wireguard.android.R
+import com.wireguard.android.databinding.ObservableKeyedRecyclerViewAdapter.RowConfigurationHandler
+import com.wireguard.android.util.ObservableKeyedList
+import com.wireguard.android.widget.ToggleSwitch
+import com.wireguard.android.widget.ToggleSwitch.OnBeforeCheckedChangeListener
+import com.wireguard.config.Attribute
+import com.wireguard.config.InetNetwork
+import com.wireguard.util.Keyed
+import java9.util.Optional
+
+/**
+ * Static methods for use by generated code in the Android data binding library.
+ */
+object BindingAdapters {
+    @JvmStatic
+    @BindingAdapter("checked")
+    fun setChecked(view: ToggleSwitch, checked: Boolean) {
+        view.setCheckedInternal(checked)
+    }
+
+    @JvmStatic
+    @BindingAdapter("filter")
+    fun setFilter(view: TextView, filter: InputFilter) {
+        view.filters = arrayOf(filter)
+    }
+
+    @BindingAdapter("items", "layout")
+    fun <E> setItems(view: LinearLayout,
+                     oldList: ObservableList<E>?, oldLayoutId: Int,
+                     newList: ObservableList<E>?, newLayoutId: Int) {
+        if (oldList === newList && oldLayoutId == newLayoutId)
+            return
+        var listener: ItemChangeListener<E>? = ListenerUtil.getListener(view, R.id.item_change_listener)
+        // If the layout changes, any existing listener must be replaced.
+        if (listener != null && oldList != null && oldLayoutId != newLayoutId) {
+            listener.setList(null)
+            listener = null
+            // Stop tracking the old listener.
+            ListenerUtil.trackListener<Any?>(view, null, R.id.item_change_listener)
+        }
+        // Avoid adding a listener when there is no new list or layout.
+        if (newList == null || newLayoutId == 0)
+            return
+        if (listener == null) {
+            listener = ItemChangeListener(view, newLayoutId)
+            ListenerUtil.trackListener(view, listener, R.id.item_change_listener)
+        }
+        // Either the list changed, or this is an entirely new listener because the layout changed.
+        listener.setList(newList)
+    }
+
+    @BindingAdapter("items", "layout")
+    fun <E> setItems(view: LinearLayout,
+                     oldList: Iterable<E>?, oldLayoutId: Int,
+                     newList: Iterable<E>?, newLayoutId: Int) {
+        if (oldList === newList && oldLayoutId == newLayoutId)
+            return
+        view.removeAllViews()
+        if (newList == null)
+            return
+        val layoutInflater = LayoutInflater.from(view.context)
+        for (item in newList) {
+            val binding = DataBindingUtil.inflate<ViewDataBinding>(layoutInflater, newLayoutId, view, false)
+            binding.setVariable(BR.collection, newList)
+            binding.setVariable(BR.item, item)
+            binding.executePendingBindings()
+            view.addView(binding.root)
+        }
+    }
+
+    @JvmStatic
+    @BindingAdapter(requireAll = false, value = ["items", "layout", "configurationHandler"])
+    fun <K, E : Keyed<out K>> setItems(view: RecyclerView,
+                                        oldList: ObservableKeyedList<K, E>?, oldLayoutId: Int,
+                                        oldRowConfigurationHandler: RowConfigurationHandler<*, *>?,
+                                        newList: ObservableKeyedList<K, E>?, newLayoutId: Int,
+                                        newRowConfigurationHandler: RowConfigurationHandler<*, *>?) {
+        if (view.layoutManager == null)
+            view.layoutManager = LinearLayoutManager(view.context, RecyclerView.VERTICAL, false)
+        if (oldList === newList && oldLayoutId == newLayoutId)
+            return
+        // The ListAdapter interface is not generic, so this cannot be checked.
+        var adapter = view.adapter as ObservableKeyedRecyclerViewAdapter<K, E>?
+        // If the layout changes, any existing adapter must be replaced.
+        if (adapter != null && oldList != null && oldLayoutId != newLayoutId) {
+            adapter.setList(null)
+            adapter = null
+        }
+        // Avoid setting an adapter when there is no new list or layout.
+        if (newList == null || newLayoutId == 0)
+            return
+        if (adapter == null) {
+            adapter = ObservableKeyedRecyclerViewAdapter(view.context, newLayoutId, newList)
+            view.adapter = adapter
+        }
+        adapter.setRowConfigurationHandler(newRowConfigurationHandler)
+        // Either the list changed, or this is an entirely new listener because the layout changed.
+        adapter.setList(newList)
+    }
+
+    @JvmStatic
+    @BindingAdapter("onBeforeCheckedChanged")
+    fun setOnBeforeCheckedChanged(view: ToggleSwitch,
+                                  listener: OnBeforeCheckedChangeListener?) {
+        view.setOnBeforeCheckedChangeListener(listener)
+    }
+
+    @BindingAdapter("android:text")
+    fun setText(view: TextView, text: Optional<*>) {
+        view.text = text.map { obj: Any -> obj.toString() }.orElse("")
+    }
+
+    @BindingAdapter("android:text")
+    fun setText(view: TextView, networks: Iterable<InetNetwork?>?) {
+        view.text = if (networks != null) Attribute.join(networks) else ""
+    }
+}

ui/src/main/java/com/wireguard/android/databinding/ObservableKeyedRecyclerViewAdapter.kt
@@ -1,0 +1,105 @@
+/*
+ * Copyright © 2017-2019 WireGuard LLC. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package com.wireguard.android.databinding
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ObservableList
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.RecyclerView
+import com.wireguard.android.BR
+import com.wireguard.android.util.ObservableKeyedList
+import com.wireguard.util.Keyed
+import java.lang.ref.WeakReference
+
+/**
+ * A generic `RecyclerView.Adapter` backed by a `ObservableKeyedList`.
+ */
+class ObservableKeyedRecyclerViewAdapter<K, E : Keyed<out K>> internal constructor(context: Context, private val layoutId: Int,
+                                                                                    list: ObservableKeyedList<K, E>?) : RecyclerView.Adapter<ObservableKeyedRecyclerViewAdapter.ViewHolder>() {
+    private val callback = OnListChangedCallback(this)
+    private val layoutInflater: LayoutInflater = LayoutInflater.from(context)
+    private var list: ObservableKeyedList<K, E>? = null
+    private var rowConfigurationHandler: RowConfigurationHandler<ViewDataBinding, Any>? = null
+
+    private fun getItem(position: Int): E? = if (list == null || position < 0 || position >= list!!.size) null else list?.get(position)
+
+    override fun getItemCount() = list?.size ?: 0
+
+    override fun getItemId(position: Int) = (getKey(position)?.hashCode() ?: -1).toLong()
+
+    private fun getKey(position: Int): K? = getItem(position)?.key
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.binding.setVariable(BR.collection, list)
+        holder.binding.setVariable(BR.key, getKey(position))
+        holder.binding.setVariable(BR.item, getItem(position))
+        holder.binding.executePendingBindings()
+        if (rowConfigurationHandler != null) {
+            val item = getItem(position)
+            if (item != null) {
+                rowConfigurationHandler?.onConfigureRow(holder.binding, item, position)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(DataBindingUtil.inflate(layoutInflater, layoutId, parent, false))
+
+    fun setList(newList: ObservableKeyedList<K, E>?) {
+        list?.removeOnListChangedCallback(callback)
+        list = newList
+        list?.addOnListChangedCallback(callback)
+        notifyDataSetChanged()
+    }
+
+    fun setRowConfigurationHandler(rowConfigurationHandler: RowConfigurationHandler<*, *>?) {
+        this.rowConfigurationHandler = rowConfigurationHandler as RowConfigurationHandler<ViewDataBinding, Any>
+    }
+
+    interface RowConfigurationHandler<B : ViewDataBinding, T> {
+        fun onConfigureRow(binding: B, item: T, position: Int)
+    }
+
+    private class OnListChangedCallback<E : Keyed<*>> constructor(adapter: ObservableKeyedRecyclerViewAdapter<*, E>) : ObservableList.OnListChangedCallback<ObservableList<E>>() {
+        private val weakAdapter: WeakReference<ObservableKeyedRecyclerViewAdapter<*, E>> = WeakReference(adapter)
+
+        override fun onChanged(sender: ObservableList<E>) {
+            val adapter = weakAdapter.get()
+            if (adapter != null)
+                adapter.notifyDataSetChanged()
+            else
+                sender.removeOnListChangedCallback(this)
+        }
+
+        override fun onItemRangeChanged(sender: ObservableList<E>, positionStart: Int,
+                                        itemCount: Int) {
+            onChanged(sender)
+        }
+
+        override fun onItemRangeInserted(sender: ObservableList<E>, positionStart: Int,
+                                         itemCount: Int) {
+            onChanged(sender)
+        }
+
+        override fun onItemRangeMoved(sender: ObservableList<E>, fromPosition: Int,
+                                      toPosition: Int, itemCount: Int) {
+            onChanged(sender)
+        }
+
+        override fun onItemRangeRemoved(sender: ObservableList<E>, positionStart: Int,
+                                        itemCount: Int) {
+            onChanged(sender)
+        }
+
+    }
+
+    class ViewHolder(val binding: ViewDataBinding) : RecyclerView.ViewHolder(binding.root)
+
+    init {
+        setList(list)
+    }
+}

phonemawhein2021/wireguard

Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>

ui/src/main/java/com/wireguard/android/databinding/BindingAdapters.kt
@@ -1,0 +1,137 @@
+/*
+ * Copyright © 2017-2019 WireGuard LLC. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package com.wireguard.android.databinding
+
+import android.text.InputFilter
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ObservableList
+import androidx.databinding.ViewDataBinding
+import androidx.databinding.adapters.ListenerUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.wireguard.android.BR
+import com.wireguard.android.R
+import com.wireguard.android.databinding.ObservableKeyedRecyclerViewAdapter.RowConfigurationHandler
+import com.wireguard.android.widget.ToggleSwitch
+import com.wireguard.android.widget.ToggleSwitch.OnBeforeCheckedChangeListener
+import com.wireguard.config.Attribute
+import com.wireguard.config.InetNetwork
+import java9.util.Optional
+
+/**
+ * Static methods for use by generated code in the Android data binding library.
+ */
+object BindingAdapters {
+    @JvmStatic
+    @BindingAdapter("checked")
+    fun setChecked(view: ToggleSwitch, checked: Boolean) {
+        view.setCheckedInternal(checked)
+    }
+
+    @JvmStatic
+    @BindingAdapter("filter")
+    fun setFilter(view: TextView, filter: InputFilter) {
+        view.filters = arrayOf(filter)
+    }
+
+    @JvmStatic
+    @BindingAdapter("items", "layout")
+    fun <E> setItems(view: LinearLayout,
+                     oldList: ObservableList<E>?, oldLayoutId: Int,
+                     newList: ObservableList<E>?, newLayoutId: Int) {
+        if (oldList === newList && oldLayoutId == newLayoutId)
+            return
+        var listener: ItemChangeListener<E>? = ListenerUtil.getListener(view, R.id.item_change_listener)
+        // If the layout changes, any existing listener must be replaced.
+        if (listener != null && oldList != null && oldLayoutId != newLayoutId) {
+            listener.setList(null)
+            listener = null
+            // Stop tracking the old listener.
+            ListenerUtil.trackListener<Any?>(view, null, R.id.item_change_listener)
+        }
+        // Avoid adding a listener when there is no new list or layout.
+        if (newList == null || newLayoutId == 0)
+            return
+        if (listener == null) {
+            listener = ItemChangeListener(view, newLayoutId)
+            ListenerUtil.trackListener(view, listener, R.id.item_change_listener)
+        }
+        // Either the list changed, or this is an entirely new listener because the layout changed.
+        listener.setList(newList)
+    }
+
+    @JvmStatic
+    @BindingAdapter("items", "layout")
+    fun <E> setItems(view: LinearLayout,
+                     oldList: Iterable<E>?, oldLayoutId: Int,
+                     newList: Iterable<E>?, newLayoutId: Int) {
+        if (oldList === newList && oldLayoutId == newLayoutId)
+            return
+        view.removeAllViews()
+        if (newList == null)
+            return
+        val layoutInflater = LayoutInflater.from(view.context)
+        for (item in newList) {
+            val binding = DataBindingUtil.inflate<ViewDataBinding>(layoutInflater, newLayoutId, view, false)
+            binding.setVariable(BR.collection, newList)
+            binding.setVariable(BR.item, item)
+            binding.executePendingBindings()
+            view.addView(binding.root)
+        }
+    }
+
+    @JvmStatic
+    @BindingAdapter(requireAll = false, value = ["items", "layout", "configurationHandler"])
+    fun <K, E : Keyed<out K>> setItems(view: RecyclerView,
+                                       oldList: ObservableKeyedArrayList<K, E>?, oldLayoutId: Int,
+                                       oldRowConfigurationHandler: RowConfigurationHandler<*, *>?,
+                                       newList: ObservableKeyedArrayList<K, E>?, newLayoutId: Int,
+                                       newRowConfigurationHandler: RowConfigurationHandler<*, *>?) {
+        if (view.layoutManager == null)
+            view.layoutManager = LinearLayoutManager(view.context, RecyclerView.VERTICAL, false)
+        if (oldList === newList && oldLayoutId == newLayoutId)
+            return
+        // The ListAdapter interface is not generic, so this cannot be checked.
+        var adapter = view.adapter as ObservableKeyedRecyclerViewAdapter<K, E>?
+        // If the layout changes, any existing adapter must be replaced.
+        if (adapter != null && oldList != null && oldLayoutId != newLayoutId) {
+            adapter.setList(null)
+            adapter = null
+        }
+        // Avoid setting an adapter when there is no new list or layout.
+        if (newList == null || newLayoutId == 0)
+            return
+        if (adapter == null) {
+            adapter = ObservableKeyedRecyclerViewAdapter(view.context, newLayoutId, newList)
+            view.adapter = adapter
+        }
+        adapter.setRowConfigurationHandler(newRowConfigurationHandler)
+        // Either the list changed, or this is an entirely new listener because the layout changed.
+        adapter.setList(newList)
+    }
+
+    @JvmStatic
+    @BindingAdapter("onBeforeCheckedChanged")
+    fun setOnBeforeCheckedChanged(view: ToggleSwitch,
+                                  listener: OnBeforeCheckedChangeListener?) {
+        view.setOnBeforeCheckedChangeListener(listener)
+    }
+
+    @JvmStatic
+    @BindingAdapter("android:text")
+    fun setText(view: TextView, text: Optional<*>) {
+        view.text = text.map { obj: Any -> obj.toString() }.orElse("")
+    }
+
+    @JvmStatic
+    @BindingAdapter("android:text")
+    fun setText(view: TextView, networks: Iterable<InetNetwork?>?) {
+        view.text = if (networks != null) Attribute.join(networks) else ""
+    }
+}

ui/src/main/java/com/wireguard/android/databinding/ObservableKeyedRecyclerViewAdapter.kt
@@ -1,0 +1,103 @@
+/*
+ * Copyright © 2017-2019 WireGuard LLC. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package com.wireguard.android.databinding
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ObservableList
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.RecyclerView
+import com.wireguard.android.BR
+import java.lang.ref.WeakReference
+
+/**
+ * A generic `RecyclerView.Adapter` backed by a `ObservableKeyedArrayList`.
+ */
+class ObservableKeyedRecyclerViewAdapter<K, E : Keyed<out K>> internal constructor(context: Context, private val layoutId: Int,
+                                                                                   list: ObservableKeyedArrayList<K, E>?) : RecyclerView.Adapter<ObservableKeyedRecyclerViewAdapter.ViewHolder>() {
+    private val callback = OnListChangedCallback(this)
+    private val layoutInflater: LayoutInflater = LayoutInflater.from(context)
+    private var list: ObservableKeyedArrayList<K, E>? = null
+    private var rowConfigurationHandler: RowConfigurationHandler<ViewDataBinding, Any>? = null
+
+    private fun getItem(position: Int): E? = if (list == null || position < 0 || position >= list!!.size) null else list?.get(position)
+
+    override fun getItemCount() = list?.size ?: 0
+
+    override fun getItemId(position: Int) = (getKey(position)?.hashCode() ?: -1).toLong()
+
+    private fun getKey(position: Int): K? = getItem(position)?.key
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.binding.setVariable(BR.collection, list)
+        holder.binding.setVariable(BR.key, getKey(position))
+        holder.binding.setVariable(BR.item, getItem(position))
+        holder.binding.executePendingBindings()
+        if (rowConfigurationHandler != null) {
+            val item = getItem(position)
+            if (item != null) {
+                rowConfigurationHandler?.onConfigureRow(holder.binding, item, position)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(DataBindingUtil.inflate(layoutInflater, layoutId, parent, false))
+
+    fun setList(newList: ObservableKeyedArrayList<K, E>?) {
+        list?.removeOnListChangedCallback(callback)
+        list = newList
+        list?.addOnListChangedCallback(callback)
+        notifyDataSetChanged()
+    }
+
+    fun setRowConfigurationHandler(rowConfigurationHandler: RowConfigurationHandler<*, *>?) {
+        this.rowConfigurationHandler = rowConfigurationHandler as? RowConfigurationHandler<ViewDataBinding, Any>
+    }
+
+    interface RowConfigurationHandler<B : ViewDataBinding, T> {
+        fun onConfigureRow(binding: B, item: T, position: Int)
+    }
+
+    private class OnListChangedCallback<E : Keyed<*>> constructor(adapter: ObservableKeyedRecyclerViewAdapter<*, E>) : ObservableList.OnListChangedCallback<ObservableList<E>>() {
+        private val weakAdapter: WeakReference<ObservableKeyedRecyclerViewAdapter<*, E>> = WeakReference(adapter)
+
+        override fun onChanged(sender: ObservableList<E>) {
+            val adapter = weakAdapter.get()
+            if (adapter != null)
+                adapter.notifyDataSetChanged()
+            else
+                sender.removeOnListChangedCallback(this)
+        }
+
+        override fun onItemRangeChanged(sender: ObservableList<E>, positionStart: Int,
+                                        itemCount: Int) {
+            onChanged(sender)
+        }
+
+        override fun onItemRangeInserted(sender: ObservableList<E>, positionStart: Int,
+                                         itemCount: Int) {
+            onChanged(sender)
+        }
+
+        override fun onItemRangeMoved(sender: ObservableList<E>, fromPosition: Int,
+                                      toPosition: Int, itemCount: Int) {
+            onChanged(sender)
+        }
+
+        override fun onItemRangeRemoved(sender: ObservableList<E>, positionStart: Int,
+                                        itemCount: Int) {
+            onChanged(sender)
+        }
+
+    }
+
+    class ViewHolder(val binding: ViewDataBinding) : RecyclerView.ViewHolder(binding.root)
+
+    init {
+        setList(list)
+    }
+}